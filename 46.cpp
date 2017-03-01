a172921b62697365cf45862c20df7a0654d9c2c4
Anshul Rai <anshul.rai7@yahoo.com>
2017-03-01 13:13:36 +0530

    Solution to #46

---
+++ 46.cpp
@@ -0,0 +1,43 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int t;
+    cin >> t;
+    vector<int> primes;
+    vector<bool> flag;
+    for(int i = 2; i<=500000; i++){
+        primes.push_back(i);
+        flag.push_back(true);
+    }
+    for(int i=2; i<=sqrt(500000); i++){
+        if(flag[i-2]){
+            for(int j = i*i; j<=500000; j+=i){
+                flag[j-2] = false;
+            }   
+        }        
+    }
+    primes.clear();
+    for(int i = 0; i<flag.size(); i++){
+        if(flag[i])
+            primes.push_back(i+2);
+    }
+    while(t--){
+        int n,count = 0;
+        cin >> n;
+        for(int i = 0; primes[i]<n; i++){
+            if((n-primes[i]) % 2 == 0){
+                int j = sqrt((n-primes[i])/2);
+                if((n-primes[i]) == 2*j*j)
+                    count++;
+            }
+        }
+        cout <<count << "\n";
+    }
+    return 0;
+}
