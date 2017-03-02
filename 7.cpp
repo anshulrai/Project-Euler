9ebaf6d4fb29af0843903de1a0712f0ffd2177a4
Anshul Rai <anshul.rai7@yahoo.com>
2017-03-02 22:49:20 +0530

    Fixed spacing

--- 7.cpp
+++ 7.cpp
@@ -6,25 +6,24 @@
 #include <limits> 
 using namespace std;
 
-
 int main() {
     vector<int> primes, primes_list;
     bool flag[1000000];
     
-    for (int i = 0; i<1000000; i++){
+    for (int i = 0; i < 1000000; i++){
         flag[i] = true;
         primes.push_back(i+2);
     }
     
-    for(int i = 0; primes[i]<=sqrt(primes.size()); i++){
+    for(int i = 0; primes[i] <= sqrt(primes.size()); i++){
         if(flag[primes[i]]){
-            for( int j = 2; primes[i]*j<= primes.size(); j++){
+            for( int j = 2; primes[i]*j <= primes.size(); j++){
                 flag[primes[i]*j] = false;
             }
         }                
     }
     
-    for(int i = 0; i<primes.size(); i++){
+    for(int i = 0; i < primes.size(); i++){
         if(flag[primes[i]])
             primes_list.push_back(primes[i]);
     }
