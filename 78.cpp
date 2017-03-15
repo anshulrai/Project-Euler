15ee52a531efb25339b18dfa81f0358baad56862
Anshul Rai <anshul.rai7@yahoo.com>
2017-03-15 20:51:58 +0530

    Incomplete Solution to #78

--- 78.cpp
+++ 78.cpp
@@ -1,31 +1,27 @@
+#pragma GCC diagnostic ignored "-Wunused-result"
 #include <cmath>
 #include <cstdio>
 #include <vector>
 #include <iostream>
 #include <algorithm>
-using namespace std;
+
 
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    int t, n;
+    scanf("%d", &t);
+    std::vector<long long> den(6e4,0), table(1+6e4,0);
+    table[0] = 1;
     
-    vector<long long> p;
-    long long val = 0;
-    p.push_back(0);
-    for(long long l = 1; l<=60000; l++){// think of making a nxn table for lookup
-        val = 0;
-        for(long long i = 1; i<=l; i++){
-            val += p[i-1]+p[l-i-1];//completely wrong
+    for(int i = 0; i<den.size(); i++){
+        den[i] = i+1;
+        for(int j = den[i]; j<=6e4;j++){
+            table[j] = (table[j] + table[j-den[i]]) % (long long)(7+1e9);
         }
-        p.push_back(val);
     }
-    int t;
-    cin >> t;
     while(t--){
-        int n;
-        cin >> n;
-        cout << p[0]<<" "<<p[3]<<" "<<p[4]<<"\n";
+        scanf("%d", &n);
+        printf("%lld\n", table[n]);
     }
     return 0;
 }
