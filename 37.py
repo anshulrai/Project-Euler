ee4704744069d151e174eb61b006dd1d2a72e4b1
Anshul Rai <anshul.rai7@yahoo.com>
2017-03-20 19:07:09 +0530

    Unoptimized Solution to #37

--- 37.py
+++ 37.py
@@ -1,17 +1,39 @@
 import math
 
-def ltr(el, size):
-    return True
+def ltr(el, psize):
+    if(psize == 1):
+        if(int(el) in tprime):
+            return True
+        else:
+            return False
+    if(ltr(el[1:], psize-1)):
+        if(int(el) in tprime):
+            return True
+        else:
+            return False
+    else:
+        return False
 
-def rtl(el, size):
-    return True
+def rtl(el, psize):
+    if(psize == 1):
+        if(int(el) in tprime):
+            return True
+        else:
+            return False
+    if(rtl(el[:-1], psize-1)):
+        if(int(el) in tprime):
+            return True
+        else:
+            return False
+    else:
+        return False
 
 n = int(input())
 prime_sum = 0
 flag = [1]*(n-1)
 tprime = []
 
-for i in range(2,int(math.sqrt(n) + 1)):
+for i in range(2,int(math.sqrt(n)) + 1):
     if flag[i-2]:
         for j in range(i*i, n+1, i):
             flag[j-2] = 0
@@ -22,10 +44,11 @@ for j in range(len(flag)):
         
 for i in range(len(tprime)):
     s = str(tprime[i])
-    if(ltr(s, len(s)) == False):
+    if(len(s) == 1 or ltr(s, len(s)) == False):
         continue
-    if(rtl(s, len(s)) == False):
-        continue    
+    if(len(s) == 1 or rtl(s, len(s)) == False):
+        continue   
+    #print(tprime[i])
     prime_sum = prime_sum + tprime[i]
 
 print(prime_sum)
